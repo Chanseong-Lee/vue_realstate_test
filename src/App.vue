5ba3b30df3e134c7003291ecbd6cfcca1b949bd3
Chanseong-Lee <mongtescu@gmail.com>
2023-04-28 17:52:28 +0900

    "add component files

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,13 @@
+<!-- 
+  동적인 ui만드는 법
+  1. UI의 현재상태를 데이터로 저장해둠 (상태 = state / data(){} 이부분을 state라고 부름)
+  2. 데이터에 따라 ui가 어떻게 보일지 작성
+-->
 <template>
+  <!-- <div v-if="1 == 2">1 == 1</div>
+  <div v-else-if="2==2">2==2</div>
+  <div v-else>else</div> -->
+  <ModalComponent/>
   <div class="menu">
     <!-- <a v-for="변수명 in 반복할 숫자" :key="변수명">Home</a> -->
     <!-- <a v-for="변수명 in 배열" :key="변수명">{{ 변수명 }}</a> -->
@@ -8,52 +17,52 @@
     <!-- <a v-for="(item, i) in menus" :key="i">{{ i }}</a> -->
     <a v-for="item in menus" :key="item">{{ item }}</a>
   </div>
-  <!-- <div v-for="a, i in 3" :key="i"> -->
-    <!-- <h4 class="red" :style="styleBinding">{{ products[i] }}</h4> 속성앞에 :을 붙이면 프로퍼티도 바인딩 가능 -->
-    <!-- <p>{{ prices[i] }} 만원 {{ a }}</p> {{data binding}} -->
-  <!-- </div> -->
-  <div>
-    <h4>{{ products[0] }}</h4>
-    <p>{{ prices[0] }}만원</p>
-    <button @click="increase(0)">허위매물신고</button> 
-    <span>신고수 : {{ report_num[0] }}</span> <!-- onClick속성은 v-on:click이고 축약하면 @click -->
+
+  <!-- 컴포넌트 호출-->
+  <DiscountComponent/>
+
+  <div v-for="(room, i) in rooms" :key="i">
+    <img :src="room.image" class="room-img">
+    <h4 @click="open_modal(i)"> {{ room.title }}</h4>
+    <p>{{ room.price }}원</p>
   </div>
-  <div>
-    <h4>{{ products[1] }}</h4>
-    <p>{{ prices[1] }}만원</p>
-    <button @click="increase(1)">허위매물신고</button> 
-    <span>신고수 : {{ report_num[1] }}</span>
-  </div>
-  <div>
-    <h4>{{ products[2] }}</h4>
-    <p>{{ prices[2] }}만원</p>
-    <button @click="increase(2)">허위매물신고</button> 
-    <span>신고수 : {{ report_num[2] }}</span>
-  </div>
+  
 </template>
 
 <script>
+import data from './data/post.js' //DB없이 해당경로에 json배열로 데이터 준비하고 import
+import DiscountComponent from './components/DiscountComponent.vue'; //component import
+import ModalComponent from './components/ModalComponent.vue'; //component import
+
 export default {
   name: 'App',
   data() {
     return {
       //{{데이터 바인딩}}을 위한 데이터 보관 : 실시간 자동 렌더링을 쓰려면 이런식으로 관리
       // 자주 변경될 데이터를 주로 보관
-      styleBinding: 'color : blue',
       menus: ["Home", "Shop", "About"],
-      products: ["역삼동원룸", "천호동원룸", "마포구원룸"],
-      prices: [60, 70, 80],
-      report_num: [0, 0, 0],
+      is_modal_opend: false, //modal control
+      rooms : data, //json array
+      clickedRoomIdx : 0,
     }
   },
   methods: {
     increase(i) {
       //report_num은 현재 블록에 정의가 안되있기때문에 data()의 report_num을 가져오기위해 this키워드를 사용. this는 상위 오브젝트를 의미
       this.report_num[i]++;
+    },
+    open_modal(i){
+      this.is_modal_opend = true;
+      this.clickedRoomIdx = i;
+    },
+    close_modal (){
+      this.is_modal_opend = false;
     }
   },
   components: {
-  }
+    DiscountComponent: DiscountComponent, //import한 컴포넌트를 등록
+    ModalComponent: ModalComponent,
+}
 }
 </script>
 
@@ -65,14 +74,49 @@ export default {
   text-align: center;
   color: #2c3e50;
 }
-
+body{
+  margin: 0px;
+}
+div{
+  box-sizing: border-box;
+}
 .menu {
   background: darkslateblue;
   padding: 15px;
   border-radius: 5px;
+  /* position: fixed; */
+  /* width: 100%; */
+  z-index: 0;
 }
 .menu a {
   color: white;
   padding: 10px;
+}
+.room-img{
+  width:100%;
+  margin-top: 40px;
+}
+.black-bg{
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  position: fixed;
+  padding: 20px;
+  z-index: 2;
+}
+.white-bg{
+  width: 100%;
+  background: white;
+  border-radius: 8px;
+  padding: 20px;
+}
+.modal_img{
+  width: 100%;
+}
+.discount{
+  background: #eee;
+  padding: 10px;
+  margin: 10px;
+  border-radius: 5px;
 }
 </style>
---
+++ src/components/ModalComponent.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="black-bg" v-if="is_modal_opend"> <!-- v-if안에 값이 참일경우에만 해당 코드를 보여줌 -->
+    <div class="white-bg">
+      <img class="modal_img" :src="rooms[clickedRoomIdx].image">
+      <h4>{{rooms[clickedRoomIdx].title}}</h4>
+      <p>{{rooms[clickedRoomIdx].content}}</p>
+      <p>{{rooms[clickedRoomIdx].price}}원</p>
+      <DiscountComponent/>
+      <button @click="close_modal">닫기</button>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+    name: "ModalComponent",
+}
+</script>
+<style>
+
+</style>
